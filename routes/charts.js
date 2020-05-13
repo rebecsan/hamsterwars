512eec7609280b81ef187f8ef3ce73e9727b751b
rebecsan <rebecca.sandelin@gmail.com>
2020-05-13 16:37:38 +0200

    '/charts work as intended'

--- routes/charts.js
+++ routes/charts.js
@@ -3,9 +3,62 @@ const { auth, db } = require('./../firebase');
 
 const router = new Router();
 
-// /charts/top GET Returnerar en array med top 5 mest vinnande hamsterobject.
+// /charts/top Return the top 5 hamsters ordered by wins (GET)
+router.get('/top', async (req, res) => {
 
-// /charts/bottom GET Returnerar en array med top 5 mest förlorande hamsterobject.
+    try {
 
+        let hamsters = [];
+        
+        // Get all hamsters from firebase
+        let snapshot = await db
+        .collection('hamsters')
+        .orderBy('wins', 'desc')
+        .limit(5)
+        .get();
+
+        snapshot.forEach(hamster => {
+            hamsters.push(hamster.data())
+        })
+
+        res.send(hamsters);
+        
+    } 
+    catch (err) {
+
+        res.status(500).send(err);
+        
+    }
+
+})
+
+// /charts/bottom Return the top 5 hamsters ordered by defeats (GET)
+router.get('/bottom', async (req, res) => {
+
+    try {
+
+        let hamsters = [];
+        
+        // Get all hamsters from firebase
+        let snapshot = await db
+        .collection('hamsters')
+        .orderBy('defeats', 'desc')
+        .limit(5)
+        .get();
+
+        snapshot.forEach(hamster => {
+            hamsters.push(hamster.data())
+        })
+
+        res.send(hamsters);
+        
+    } 
+    catch (err) {
+
+        res.status(500).send(err);
+        
+    }
+
+})
 
 module.exports = router;
